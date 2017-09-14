9897389b0b000564be8b52dd34d03bf8f0954c9c
ciara hudson <cchudson33@gmail.com>
2017-09-14 22:45:40 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,9 +0,0 @@
-function sayHappyHolidays(string){
-  return string('Happy Holidays!')
-}
-
-string.sayHappyHolidays
-
-function log.sayHappyHolidays(string){
-  return console.log(string('Happy Holidays'))
-}
