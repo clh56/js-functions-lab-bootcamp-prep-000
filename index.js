0200b273077bb51d2ada08af4d8ee36fffcb252c
ciara hudson <cchudson33@gmail.com>
2017-09-14 22:54:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!` ;
 };
 
-function holidayCountdown(holiday,number) {
+function holidayCountdown(holiday, number) {
   return `It\'s ${number} days until ${holiday} !` ;
 };
